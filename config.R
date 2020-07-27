58c6408d04e4c171d9409ebdbf159ecf52e38310
jbgraille@gmail.com <jbgraille@gmail.com>
2020-07-27 10:05:15 +0200

    init

---
+++ config.R
@@ -0,0 +1,17 @@
+# Clean up workspace
+rm(list = ls(all.names = TRUE))
+# Set environnement
+if (!('renv' %in% installed.packages())) install.packages('renv')
+library(renv)
+# for the first run uncomment the row below
+# renv::init() 
+# Install the packages
+required.packages = c("shiny","tidyverse","remotes","rjson","shinyWidgets","shinyjs","rhandsontable")
+new.packages = required.packages[!(required.packages %in% installed.packages()[,"Package"])]
+if (length(new.packages)) install.packages(new.packages, dependencies = TRUE, repos = "http://cran.us.r-project.org", lib=.libPaths()[1])
+lapply(required.packages, function(x){
+  print(x)
+  require(x, character.only = TRUE)
+})
+# renv::snapshot()
+rm(required.packages, new.packages)
